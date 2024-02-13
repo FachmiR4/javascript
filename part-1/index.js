2c651be963d89bb13b5ff45b7903ab3ff811fd2c
Fachmi Ramadhan <fahmiramadhan123.fr@gmail.com>
2024-02-13 20:47:07 +0700

    melakukan perubahan untuk belajar javascript part-1

---
+++ part-1/index.js
@@ -0,0 +1,5 @@
+console.log('hello'); 
+window.alert("ini sebuah peringatan");
+
+document.getElementById('myH').textContent = "hello";
+document.getElementById('myP').textContent = "saya suka belajar programing";
