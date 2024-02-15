d33faf810f09b16671ea57c016ccebcc4b9a57f0
Fachmi Ramadhan <fahmiramadhan123.fr@gmail.com>
2024-02-15 22:13:04 +0700

    belajar variable di javascript part-2

--- part-1/index.js
+++ part-1/index.js
@@ -3,3 +3,36 @@ window.alert("ini sebuah peringatan");
 
 document.getElementById('myH').textContent = "hello";
 document.getElementById('myP').textContent = "saya suka belajar programing";
+
+//variable 
+
+// variable = a countainer that a value. 
+//              behaves as if it was the value it contains.
+
+// 1. declaration  let x;
+// 2. assignment x = 100;
+
+let age1 = 25; // declaration & value
+let price = 10.23;  // declaration & value
+let nama; // delcaration
+nama = "fahmi ramadhan"; // value
+let forSale = true; // boolean
+
+output in terminal
+console.log(nama); 
+console.log(typeof forSale);
+console.log(typeof nama);
+console.log(typeof age1);
+console.log(`your age is ${age} years old`);
+console.log(`the price is $${price}`);
+
+let fullNama = "fachmi ramadhan";
+let age2 = 25;
+let isStudent = false;
+
+document.getElementById('myNama').textContent = `your name is ${fullNama}`;
+document.getElementById('myAge').textContent = `you are ${age2} years old`;
+document.getElementById('Student').textContent = `Enrolled : ${isStudent}`;
+
+
+
