d0ba08f1fb41937ead4fe770f8d4be5cfad26413
Fachmi Ramadhan <fahmiramadhan123.fr@gmail.com>
2024-02-15 22:43:42 +0700

    belajar artimatika operator di javascript part-3

--- part-1/index.js
+++ part-1/index.js
@@ -4,7 +4,7 @@ window.alert("ini sebuah peringatan");
 document.getElementById('myH').textContent = "hello";
 document.getElementById('myP').textContent = "saya suka belajar programing";
 
-//variable 
+/*--------------------------------variable---------------------------------------------- */
 
 // variable = a countainer that a value. 
 //              behaves as if it was the value it contains.
@@ -18,13 +18,13 @@ let nama; // delcaration
 nama = "fahmi ramadhan"; // value
 let forSale = true; // boolean
 
-output in terminal
-console.log(nama); 
-console.log(typeof forSale);
-console.log(typeof nama);
-console.log(typeof age1);
-console.log(`your age is ${age} years old`);
-console.log(`the price is $${price}`);
+// output in terminal
+// console.log(nama); 
+// console.log(typeof forSale);
+// console.log(typeof nama);
+// console.log(typeof age1);
+// console.log(`your age is ${age} years old`);
+// console.log(`the price is $${price}`);
 
 let fullNama = "fachmi ramadhan";
 let age2 = 25;
@@ -33,6 +33,40 @@ let isStudent = false;
 document.getElementById('myNama').textContent = `your name is ${fullNama}`;
 document.getElementById('myAge').textContent = `you are ${age2} years old`;
 document.getElementById('Student').textContent = `Enrolled : ${isStudent}`;
+
+/*--------------------------------Arithmetic----------------------------------------------*/
+
+// arithmetic operator = (value, variable, atc)
+//                      operators (+ - * /)
+//                      ex. 11 = x + 5;
+
+
+
+let valueStudens = 30;
+
+valueStudens = valueStudens + 2;
+valueStudens = valueStudens - 3;
+valueStudens = valueStudens * 2;
+valueStudens = valueStudens ** 2;
+let extraValue = valueStudens % 2;
+valueStudens /= 2;
+valueStudens **= 3;
+
+valueStudens++;
+valueStudens--;
+--valueStudens;
+++valueStudens;
+
+/*
+  operator precedence
+  1. paranthesis ()
+  2. exponents
+  3. multplication & divition & modulo
+  4. addtion & subtraction  
+*/
+
+let pratice = 12 + 2 % (3 - 1);
+
 
 
 
