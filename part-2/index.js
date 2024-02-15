efdcf02ff917e94ecaf0e2329effa71f0848123a
Fachmi Ramadhan <fahmiramadhan123.fr@gmail.com>
2024-02-16 00:21:10 +0700

    belajar variable constant di javascript part-5

--- part-2/index.js
+++ part-2/index.js
@@ -13,6 +13,30 @@ x = Number(x);
 y = String(y);
 z = Boolean(z);
 
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+/*------------------------constant------------------------------*/
+
+// const = a variable that can't be changed
+
+const PI = 3.14159; // variable constant harus mengunakan huruf besar
+let radius;
+let circumference;
+
+
+// radius = window.prompt("enter the radius of circle");
+
+
+
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference + "cm";
+
+}
+
+
+
