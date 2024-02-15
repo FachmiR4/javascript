69388bab99dac5f5179f694192065cba113ec510
Fachmi Ramadhan <fahmiramadhan123.fr@gmail.com>
2024-02-15 23:50:36 +0700

    belajar conversion dijavascript part-4

--- part-1/index.js
+++ part-1/index.js
@@ -1,5 +1,5 @@
 console.log('hello'); 
-window.alert("ini sebuah peringatan");
+// window.alert("ini sebuah peringatan");
 
 document.getElementById('myH').textContent = "hello";
 document.getElementById('myP').textContent = "saya suka belajar programing";
---
+++ part-2/index.js
@@ -0,0 +1,18 @@
+// type conversion = change the datatype of a value to another
+//                   (strings, numbers, booleans)
+
+// let age = window.prompt("how old are you");
+// age = Number(age);
+// age+=1;
+
+let x = "0";
+let y = "saya";
+let z = "saya";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
