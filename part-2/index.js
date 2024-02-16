70aebe241b1f9ee1dcafdc977dd3354bb2fe41d8
Fachmi Ramadhan <fahmiramadhan123.fr@gmail.com>
2024-02-17 02:53:41 +0700

    belajar matematika di javascript part-7

--- part-2/index.js
+++ part-2/index.js
@@ -39,4 +39,33 @@ document.getElementById("mySubmit").onclick = function(){
 }
 
 
+/*-------------------------------------------------math------------------------------------*/
+
+// math = built-in object that provide a collection of properties and methods
+            // collection of properties and methods
+
+
+// console.log(Math.PI);
+// console.log(Math.E)
+
+let a = 45;
+let d = 10;
+let e = 1;
+
+z = Math.round(a); // mengubah menjadi bilangan bulat
+z = Math.floor(a); // mengubah bialng bulat kebawah 3.99 = 3
+z = Math.ceil(a); // mengubah bilang bulat ke atas 3.25 = 4
+z = Math.trunc(a); // mengubah bilang bulat bedasarkan dari angka depan 3.111 = 3 & 3.999 = 3
+z = Math.pow(d, a); // mempangkatkan sebuah nilai 2**3 = 8
+z = Math.sqrt(a); //mencari akar kuadrat dari sebuah nilai 81 = 9
+z = Math.log(a); // mencari logaritma natural dari sebuah nilai 10 = 2.302
+z = Math.sin(a); // mendapatkan sebuah nilai radian dri sebuah nilai 45 = 0.85
+z = Math.cos(a); // mendapatkan nilai costanta bedasarkan nilai 45 = 0.52
+z = Math.tan(a); // mendapatkan sebuah nilai tan bedasarkan nilai 45 = 1.61
+z = Math.abs(a); // mendapaikan nilai absolute bedasarkan nilai 45 = 45
+z = Math.sign(a) // mendapatkan niali awal dari sebuah nilai 45 = 1, 0 = 0, -45 = -1;
+let max = Math.max(a,d,e); // mendapatkan nilai max pada suatu nilai 45, 10, 1 = 45
+let min = Math.min(a,d,e); // mendapatkan sebuah nilai min berdasarkan nilai 45,10,1 =1
+
+
 
