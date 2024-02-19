fc5bb3a19a0cbfff607de53b94a82e4ce125b8e3
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-19 22:46:40 +0700

    belajar strict equality by javascript part-17

--- part-4/index.js
+++ part-4/index.js
@@ -1,25 +1,25 @@
 // methods Chaining = Calling one method after another 
 //                     in one continuous line of code.
 
-// let username = window.prompt("enter your username:");
+let username = window.prompt("enter your username:");
 
 // ------------NO METHOD CHAINING--------------
 
-// username = username.trim();
-// let letter = username.charAt(0);
-// letter = letter.toUpperCase();
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
 
-// let extraChars = username.slice(1);
-// extraChars = extraChars.toLowerCase();
-// username = letter + extraChars;
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase();
+username = letter + extraChars;
 
-// // console.log(username);
+console.log(username);
 
 // // -----------METHOD CHAINING---------------
 
-// username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
 
-// console.log(username);
+console.log(username);
 
 
 /*-----------------------------------Logical Operator------------------------------------------*/
@@ -38,4 +38,20 @@ if(!isSunny){
 }
 else{
     console.log("it is Not cloudy"); // output
+}
+
+/*-----------------------------------strict equality------------------------------------------*/
+
+//      = assignment operator
+//     == comparasion operator (compare if value are equal)
+//    === strict equality operator (compare if value & datatype are aqual)
+//     != inequality operator
+//    !== strict inequality operator
+
+const PI = 3.14;
+
+if(PI !== "3.14"){
+    console.log("that is not pi");
+}else{
+    console.log("that is pi");
 }
