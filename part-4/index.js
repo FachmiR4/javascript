281b1388108f3809f108d12e11f776ba2050f434
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-19 22:29:54 +0700

    belajar logical operator by javascript part-16

--- part-4/index.js
+++ part-4/index.js
@@ -1,22 +1,41 @@
 // methods Chaining = Calling one method after another 
 //                     in one continuous line of code.
 
-let username = window.prompt("enter your username:");
+// let username = window.prompt("enter your username:");
 
 // ------------NO METHOD CHAINING--------------
 
-username = username.trim();
-let letter = username.charAt(0);
-letter = letter.toUpperCase();
+// username = username.trim();
+// let letter = username.charAt(0);
+// letter = letter.toUpperCase();
 
-let extraChars = username.slice(1);
-extraChars = extraChars.toLowerCase();
-username = letter + extraChars;
+// let extraChars = username.slice(1);
+// extraChars = extraChars.toLowerCase();
+// username = letter + extraChars;
+
+// // console.log(username);
+
+// // -----------METHOD CHAINING---------------
+
+// username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
 
 // console.log(username);
 
-// -----------METHOD CHAINING---------------
 
-username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+/*-----------------------------------Logical Operator------------------------------------------*/
 
-console.log(username);
+// logical operator = used to combine or manipulate boolean value
+//                     (true or false)
+
+//                     AND = &&
+//                     OR = ||
+//                     NOT = !
+
+const isSunny = true;
+
+if(!isSunny){
+    console.log("it is cloudy");
+}
+else{
+    console.log("it is Not cloudy"); // output
+}
