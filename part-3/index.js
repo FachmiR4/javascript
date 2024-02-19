42d2f3b57bc780c0fa6222f069f696578fa57fb4
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-19 20:00:05 +0700

    belajar switch case by javascript part-12

--- part-3/index.js
+++ part-3/index.js
@@ -13,4 +13,57 @@ let pesan = isStudent ? "you are a student" : "you are not student";
 
 let purchaseAmount = 99;
 let discount = purchaseAmount >= 100 ? 10 : 0;
-console.log(`your total id ${purchaseAmount - purchaseAmount * (discount/100)}`)
+// console.log(`your total id ${purchaseAmount - purchaseAmount * (discount/100)}`)
+
+/*------------------------------------Switch---------------------------------------*/
+// switch = can be an affecient replacement to many else if statements
+
+// let day = 1;
+
+switch(day){
+    case 1:
+        console.log("ini hari senin");
+        break;
+    case 2:
+        console.log("ini hari selasa");
+        break;
+    case 3:
+        console.log("ini hari rabu");
+        break;
+    case 4:
+        console.log("ini hari kamis");
+        break;
+    case 5:
+        console.log("ini hari jum'at");
+        break;
+    case 6:
+        console.log("ini hari sabtu");
+        break;
+    case 7:
+        console.log("ini hari minggu");
+        break;
+    default:
+        console.log(`${day} ini bukan hari`);
+}
+
+let testScore = 5;
+let letterGrade;
+
+switch(true){
+    case testScore >= 90:
+        letterGrade = "A";
+        break;
+    case testScore >= 70:
+        letterGrade = "B";
+        break;
+    case testScore >= 60:
+        letterGrade = "C";
+        break;
+    case testScore >= 50:
+        letterGrade = "D";
+        break;
+    default:
+        letterGrade = "E";
+}
+
+console.log(letterGrade);
