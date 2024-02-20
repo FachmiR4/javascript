599673a6b3e7402a953d7214a7560f6569d4a241
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-20 07:32:33 +0700

    belajar function by javascript part-21

--- part-4/index.js
+++ part-4/index.js
@@ -1,25 +1,25 @@
 // methods Chaining = Calling one method after another 
 //                     in one continuous line of code.
 
-let username = window.prompt("enter your username:");
+// let username = window.prompt("enter your username:");
 
 // ------------NO METHOD CHAINING--------------
 
-username = username.trim();
-let letter = username.charAt(0);
-letter = letter.toUpperCase();
+// username = username.trim();
+// let letter = username.charAt(0);
+// letter = letter.toUpperCase();
 
-let extraChars = username.slice(1);
-extraChars = extraChars.toLowerCase();
-username = letter + extraChars;
+// let extraChars = username.slice(1);
+// extraChars = extraChars.toLowerCase();
+// username = letter + extraChars;
 
-console.log(username);
+// console.log(username);
 
 // // -----------METHOD CHAINING---------------
 
-username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+// username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
 
-console.log(username);
+// console.log(username);
 
 
 /*-----------------------------------Logical Operator------------------------------------------*/
@@ -31,14 +31,14 @@ console.log(username);
 //                     OR = ||
 //                     NOT = !
 
-const isSunny = true;
+// const isSunny = true;
 
-if(!isSunny){
-    console.log("it is cloudy");
-}
-else{
-    console.log("it is Not cloudy"); // output
-}
+// if(!isSunny){
+//     console.log("it is cloudy");
+// }
+// else{
+//     console.log("it is Not cloudy"); // output
+// }
 
 /*-----------------------------------strict equality------------------------------------------*/
 
@@ -48,10 +48,40 @@ else{
 //     != inequality operator
 //    !== strict inequality operator
 
-const PI = 3.14;
+// const PI = 3.14;
 
-if(PI !== "3.14"){
-    console.log("that is not pi");
-}else{
-    console.log("that is pi");
+// if(PI !== "3.14"){
+//     console.log("that is not pi");
+// }else{
+//     console.log("that is pi");
+// }
+
+/*-----------------------------------Function------------------------------------------*/
+
+// function = A section of reusable code.
+//            Declare code once, used it whenever you want.
+//            Call the function to execute that code.
+//            function name_function(parameters){argument}
+
+function add(x , y){
+    return x + y;
+}
+function subtract(x, y){
+    return x - y;
+}
+function multiply(x, y){
+    return x * y;
+}
+function divide(x, y){
+    return x / y;
+}
+function isEven(number){
+    return number % 2 === 0 ? true : false;
+}
+function isValidEmail(email){
+    if(email.includes("@")){
+        return true;
+    }else{
+        return false;
+    }
 }
