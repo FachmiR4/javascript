896ccab3d20581e4178bd089036841e82a6d5815
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-20 15:54:07 +0700

    belajar array by javascript part-24

--- part-5/index.js
+++ part-5/index.js
@@ -13,3 +13,25 @@ function function2(){
     let x = 4; //variable local yang dapat diakses tempat tertentu
     console.log(x);
 }
+
+/*-----------------------------------Array------------------------------------------*/
+// array = a variable like structure that can hold more than 1 more
+
+let fruits = ["apple", "orange", "banana","cerry"];
+
+fruits.push("coconat"); //untuk menambahkan data baru ke array
+fruits.pop(); // untuk menghapus data trakhir disebuah array
+fruits.unshift("manggo") //untuk menambahkan data baru diindex pertama ke sebuah array
+fruits.shift(); // menghapus data pertama disebuah array
+
+let numOfFruits = fruits.length;
+let index = fruits.indexOf("");
+
+// sort() digunakan untuk mengurutkan alfabet dari a-z
+// reverse() digunakan untuk kebalikanya
+
+fruits.sort().reverse(); 
+
+for(let fruit of fruits){
+    console.log(fruit);
+}
