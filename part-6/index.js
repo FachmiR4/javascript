e52c76bc85f5ff28fd1ca165233d99d667496aea
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-21 08:54:48 +0700

    belajar methods .forEach() by javascript part-30

--- part-6/index.js
+++ part-6/index.js
@@ -34,3 +34,48 @@ function displayConsole(result){
 function displayPage(result){
     document.getElementById("myH1").textContent = result;
 }
+
+/*-----------------------------------ForEach------------------------------------------*/
+
+// forEach() = metrhod use to iterate over the element
+//             of an array and apply a specified function (callback)
+//             to each element
+
+//             array.forEach(callback)
+//             element, index, array are provided
+
+let number = [1,2,3,4,5];
+
+// number.forEach(squere); // callback with forEach
+// number.forEach(display); // output display
+
+function double(element, index, array){
+    array[index] = element * 2;
+}
+function triple(element, index, array){
+    array[index] = element * 3;
+}
+function squere(element, index, array){
+    array[index] = Math.pow(element, 2);
+}
+function display(element){
+    console.log(element);
+}
+
+let fruits = ["banana", "Coconat", "ORANGE", "apple"];
+
+fruits.forEach(upperCase);
+fruits.forEach(display1);
+
+function lowerCase(element, index, array){
+    array[index] = element.toLowerCase();
+}
+function upperCase(element, index, array){
+    array[index] = element.toUpperCase();
+}
+function capitalize(element, index, array){
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1); 
+}
+function display1(element){
+    console.log(element)
+}
