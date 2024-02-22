6e91d3fb82675c47ed289acb0b32a1c9507816cb
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-22 16:02:08 +0700

    belajar static by javascript part-39

--- part-8/index.js
+++ part-8/index.js
@@ -63,3 +63,52 @@ const salesTax = 0.05;
 
 const total = product1.calculateTotal(salesTax);
 // console.log(`Total price (with tax): $${total.toFixed(2)}`);
+
+/*-----------------------------------static------------------------------------------*/
+
+// static = keyword that defines properties or methods that belong
+//          to a class itself rather than the object created
+//          from that class (class owns anything static, not the object)
+
+class methUntil{
+    static PI = 3.14159;
+
+    static getDiameter(radius){
+        return radius * 2;
+    }
+
+    static getCircumference(radius){
+        return 2 * this.PI * radius;
+    }
+    static getArea(radius){
+        return this.PI * radius * radius;
+    }
+}
+
+// console.log(methUntil.PI);
+// console.log(methUntil.getDiameter(10));
+// console.log(methUntil.getCircumference(10))
+// console.log(methUntil.getArea(10))
+
+class User{
+    static userCount = 0;
+
+    constructor(username){
+        this.username = username;
+        User.userCount++;
+    }
+
+    static getUserCount(){
+        console.log(`there are ${User.userCount} users online`)
+    }
+    sayHeloo(){
+        console.log(`Hello, my username is ${this.username}`)
+    }
+}
+
+const user1 = new User("fachmi");
+
+// console.log(user1.username);
+// console.log(User.userCount);
+console.log(user1.sayHeloo());
+User.getUserCount();
