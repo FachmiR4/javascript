7474eace066ffa164bf12d8d59139414958cece1
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-23 23:36:43 +0700

    belajar Date Object by javascript part-48

--- part-10/index.js
+++ part-10/index.js
@@ -125,3 +125,30 @@ function Shuffle(array){
         [array[i], array[random]] = [array[random], array[i]]
     }
 }
+
+/*-----------------------------------Dates------------------------------------------*/
+// Date Object = Object that contain values that represent dates and times 
+//               these date objects can be changed and formatted
+
+// Date(year, month, day, hour, minute, second, ms)
+
+// const date = new Date(2024, 0, 1, 2, 23, 0, 5);
+// const date = new Date("2024-01-02T12:00:00Z");
+// const date = new Date(1700000000000);
+
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const dayOfWeek = date.getDay();
+
+console.log(day);
+console.log(dayOfWeek);
+
+const date1 = new Date("2023-12-31");
+const date2 = new Date("2024-01-01");
+
+if(date2 > date1){
+    console.log("heppy new year")
+}
