5fc755fac88a2f69e31af6dec221d97002b76021
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-25 16:29:15 +0700

    belajar add & change Element HTML by javaScript part-60

--- part-12/index.js
+++ part-12/index.js
@@ -98,3 +98,53 @@ Array.from(foods).forEach(food => {
 // const element = document.getElementById("fruits");
 // const childrenElement = element.children;
 // childrenElement[1].style.backgroundColor = "yellow";
+
+
+/*----------------------------------Add & Change HTML------------------------------------------*/
+
+// -------------- EXAMPLE 1 <H1> ----------
+// 1. STEP 1 CREATE THE ELEMENT
+// const newH1 = document.createElement("h1");
+
+// 2. STEP 2 ADD ATTRIBUTES/PROPERTIES
+// newH1.textContent = "I Like Pizza!";
+// newH1.id = "myH1";
+// newH1.style.color = "tomato";
+// newH1.style.textAlign = "center"
+
+// 3. STEP 3 APPEND ELEMENT TO DOM
+// document.body.append(newH1);
+// document.body.prepend(newH1)
+// document.getElementById("box1").append(newH1);
+// document.getElementById("box1").prepend(newH1);
+
+// const box2 = document.getElementById("box2");
+// document.body.insertBefore(newH1, box2)
+
+// const boxes = document.querySelectorAll(".box")
+// document.body.insertBefore(newH1, boxes[3]);
+
+// REMOVE HTML ELEMENT
+// document.body.removeChild(newLink);
+// document.getElementById("box1").removeChild(newH1);
+
+
+// -------------- EXAMPLE 2 <li> ----------
+// 1. STEP 1 CREATE THE ELEMENT
+const newList = document.createElement("li");
+
+// 2. STEP 2 ADD ATTRIBUTES/PROPERTIES
+newList.textContent = "Coconat";
+newList.id = "Coconut";
+newList.style.fontWeight = "Bold";
+newList.style.backgroundColor = "lightgreen";
+
+// 3. STEP 3 APPEND ELEMENT TO DOM
+// document.body.append(newList);
+// document.body.prepend(newList);
+// document.getElementById("fruits").append(newList);
+document.getElementById("fruits").prepend(newList);
+
+
+// REMOVE HTML ELEMENT
+
