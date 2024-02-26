58951a0d67df03fe5b5a6dc27421b7c242560f03
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-02-26 14:10:57 +0700

    belajar eventListerner (key events) by javascript part-63

--- Mouse_events/index.js
+++ Mouse_events/index.js
@@ -2,9 +2,9 @@
 //                  events: click, mouseover, mouseout
 //                  .addEventListener(event, callback);
 
-const myBox = document.getElementById("myBox");
-const myButton = document.getElementById("myButton");
-let clickActive = false;
+// const myBox = document.getElementById("myBox");
+// const myButton = document.getElementById("myButton");
+// let clickActive = false;
 
 // myBox.addEventListener("click", event => {
 //     event.target.style.backgroundColor = "tomato";
@@ -19,29 +19,70 @@ let clickActive = false;
 //     event.target.textContent = "Click Me 😄";
 // })
 
-myButton.addEventListener("mouseover", () => {
-    if(!clickActive){
-        myBox.style.backgroundColor = "yellow";
-        myBox.textContent = "Don't do it! 😢";
-        clickActive = false;
-    }
+// myButton.addEventListener("mouseover", () => {
+//     if(!clickActive){
+//         myBox.style.backgroundColor = "yellow";
+//         myBox.textContent = "Don't do it! 😢";
+//         clickActive = false;
+//     }
+// })
+// myButton.addEventListener("mouseout", () => {
+//     if(!clickActive){
+//         myBox.style.backgroundColor = "lightgreen";
+//         myBox.textContent = "Click Me 😄";
+//         clickActive = false;
+//     }
+// })
+// myButton.addEventListener("click", () => {
+//     if(!clickActive){
+//         myBox.style.backgroundColor = "tomato";
+//         myBox.textContent = "OUCH! 😒";
+//         clickActive = true;
+//     }
+//     else if(clickActive){
+//         myBox.style.backgroundColor = "lightgreen";
+//         myBox.textContent = "Click Me 😄";
+//         clickActive = false;
+//     }
+// });
+
+
+/*-----------------------------------key Events------------------------------------------*/
+//  events: keydown, keyup. keypress
+
+const myBox = document.getElementById("myBox");
+const moveAmount = 10;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", events => {
+    myBox.style.backgroundColor = "tomato";
+    myBox.textContent = "🤨";
 })
-myButton.addEventListener("mouseout", () => {
-    if(!clickActive){
-        myBox.style.backgroundColor = "lightgreen";
-        myBox.textContent = "Click Me 😄";
-        clickActive = false;
-    }
+document.addEventListener("keyup", events => {
+    myBox.style.backgroundColor = "lightblue";
+    myBox.textContent = "😄";
+    console.log(events.key)
 })
-myButton.addEventListener("click", () => {
-    if(!clickActive){
-        myBox.style.backgroundColor = "tomato";
-        myBox.textContent = "OUCH! 😒";
-        clickActive = true;
-    }
-    else if(clickActive){
-        myBox.style.backgroundColor = "lightgreen";
-        myBox.textContent = "Click Me 😄";
-        clickActive = false;
-    }
-});
+
+document.addEventListener('keydown', events => {
+        if(events.key.startsWith("Arrow")){
+            events.preventDefault()
+            switch(events.key){
+                case "ArrowUp":
+                    y -= moveAmount;
+                    break;
+                case "ArrowDown":
+                    y += moveAmount;
+                    break;
+                case "ArrowLeft":
+                    x -= moveAmount;
+                    break;
+                case "ArrowRight":
+                    x += moveAmount;
+                    break;
+            }
+        }
+        myBox.style.top = `${y}px`;
+        myBox.style.left = `${x}px`;
+})
